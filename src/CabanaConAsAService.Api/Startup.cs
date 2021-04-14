ece2f10e92741333397528f4230db93ca31236d3
Sean McGettrick <smcgettrick@gmail.com>
2021-04-13 20:52:55 -0400

    Revert "Add AppInsights"
    This reverts commit e3ac99080849421abd8d75abfb07b0630ab67d04.

--- src/CabanaConAsAService.Api/Startup.cs
+++ src/CabanaConAsAService.Api/Startup.cs
@@ -29,7 +29,6 @@ namespace CabanaConAsAService.Api
             });
 
             services.AddMediatR(typeof(GetCabanaConTimeHandler).GetTypeInfo().Assembly);
-            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
